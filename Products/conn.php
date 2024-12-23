4554762c32967635a6ec2d1e51918b75d43b9d9e
jeanaimeiraguha <jeanaimeiraguha@gmail.com>
2024-12-23 09:45:55 +0200

    INv

--- Products/conn.php
+++ Products/conn.php
@@ -1,10 +1,4 @@
 <?php
 $conn=mysqli_connect('localhost','root','','Shirinyota');
-if ($conn) {
-    # code...
-    echo "connected successfully";
-}
-else{
-    echo "Connection failed";
-}
+
 ?>
--- Products/select.php
+++ Products/select.php
@@ -3,3 +3,29 @@ include('conn.php');
 $select=mysqli_query($conn, "SELECT * FROM products");
 while($row=mysqli_fetch_array($select)){
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <table border="1">
+        <tr>
+            <th>Product Id</th>
+            <th>Product Name</th>
+            <th colspan="2">Operations</th>
+        </tr>
+        <tr>
+            <td><?php echo $row['Product_id']?></td>
+            <td><?php echo $row['Product_Name']?></td>
+            <td><a href=""></a></td>
+            <td><a href=""></a></td>
+        </tr>
+        <?php
+        }?>
+    </table>
+
+</body>
+</html>
