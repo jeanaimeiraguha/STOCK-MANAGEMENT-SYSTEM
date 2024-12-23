70a23207a2e6133756f1f1165d15765f102dc446
jeanaimeiraguha <jeanaimeiraguha@gmail.com>
2024-12-23 10:00:43 +0200

    INv

--- Products/update.php
+++ Products/update.php
@@ -1,5 +1,30 @@
 <?php
-$Product_id=$_GET['Product_id'];
 
+$Product_id=$_GET['Product_id'];
+$select=mysqli_query($conn,"SELECT * FROM products WHERE Product_id='$Product_id'");
+$row=mysqli_fetch_array($select);
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="post">
+        
+    </form>
+</body>
+</html>
+
+
+
+<?php
+
+
+
+$update=mysqli_query($conn, "UPDATE products SET Product_id='$Product_id',Product_name='$Product_id' WHERE Product_id='$Product_id'");
 
 ?>
