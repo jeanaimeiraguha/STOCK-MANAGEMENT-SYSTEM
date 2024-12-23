11cc20c46af736fe52543d5e1810a1cdbb4c8acf
jeanaimeiraguha <jeanaimeiraguha@gmail.com>
2024-12-23 10:09:14 +0200

    INv

--- Products/update.php
+++ Products/update.php
@@ -3,9 +3,10 @@ if (isset($_GET['Product_id'])) {
     # code...
 
 $Product_id=$_GET['Product_id'];
-}
+
 $select=mysqli_query($conn,"SELECT * FROM products WHERE Product_id='$Product_id'");
 $row=mysqli_fetch_array($select);
+}
 
 ?>
 <!DOCTYPE html>
