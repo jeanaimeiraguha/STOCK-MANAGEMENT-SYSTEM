48f934c637b416089c5771ce15117e2a5e96185f
jeanaimeiraguha <jeanaimeiraguha@gmail.com>
2024-12-24 21:35:39 +0200

    Stock  Management system

--- Shirinyota/includes/stock_in.php
+++ Shirinyota/includes/stock_in.php
@@ -1,4 +1,19 @@
 <?php
+
+$servername = "localhost";
+$username = "root"; // Default MySQL username
+$password = ""; // Default password is empty for XAMPP
+$dbname = "shirinyota"; // The name of the database
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error); // If connection fails, show an error
+}
+
+
 // Start the session to ensure the user is logged in
 session_start();
 
